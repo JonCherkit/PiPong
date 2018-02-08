24db7df7d76629b46e74a59a26d8b1a5f2a6a729
Jonathan CHERKIT <j.cherkit@gmail.com>
2018-02-08 16:57:25 +0100

    Affichage du fond de la zone de jeu OK.

--- Bricks.js
+++ Bricks.js
@@ -2,27 +2,27 @@ import { Brick } from './Brick.js';
 
 'use strict';
 
-export class Bricks extends Brick
+export class Bricks
 {
-	constructor(x,y)
+	constructor()
 	{
-		super(x,y);
+
 		this._brickList = [];
 		
 	}
 
 	init(n = 3,m = 3)
 	{
-		let spaceAroundX = 500 - n*this._width;
+		let brick = new Brick();
+		let spaceAroundX = 500 - n*brick.getWidth();
 		let horiSpace = spaceAroundX/n+1;
-		let spaceAroundY = 500*1/2 - n*this._height;
+		let spaceAroundY = 500*1/2 - n*brick.getHeight();
 		let vertSpace = spaceAroundY/m;
 		for(let i = 0; i< n;i++)
 		{
 			for(let j = 0; j < m ; j++)
 			{
-				console.log(i*this.getWidth()+spaceAroundX);
-				this._brickList.push(new Brick(i*this.getWidth()+spaceAroundX,j*this.getHeight()+spaceAroundY));
+				this._brickList.push(new Brick(i*brick.getWidth()+spaceAroundX,j*brick.getHeight()+spaceAroundY));
 			}
 		}
 		return(this);
--- main.js
+++ main.js
@@ -4,6 +4,24 @@ import {Bricks} from './Bricks.js';
 'use strict';
 
 document.addEventListener('DOMContentLoaded',function(){
-	let bricks = new Bricks();
-	console.log(bricks.init());
+	let bricks = new Bricks().init();
+	//--- Canvas Manipulation
+	// 1 - Game display
+	const canvas = document.querySelector('#Pong')
+	const ctx = canvas.getContext('2d');
+	drawBackGround(ctx,canvas.offsetWidth,canvas.offsetHeight);
 });
+
+function drawBackGround(ctx,width,height)
+{
+	//Couleur Grey/Gris Background
+	ctx.fillStyle = '#B8B8B8';
+	//Remplissage
+	ctx.fillRect(0,0,width,height);
+}
+
+/** RAF
+ * 1 - finir le rendu des elements dans canevas
+ * 2 - gérer l'interactivité ( gauche, droite, départ, fin, collision, trajectoire, ...)
+ * 3 - gestion cycle de jeu ( nb de vie, cdt victoire/défaite )
+ */
